a86fc997d4e8d6a5a907d1ab6b65843f7c90ddca
Eric Milford <ericmilford@gmail.com>
2023-11-21 18:27:29 -0600

    Configure RSpec in `after_initialize` block

--- lib/capybara_accessibility_audit/engine.rb
+++ lib/capybara_accessibility_audit/engine.rb
@@ -18,19 +18,21 @@ module CapybaraAccessibilityAudit
       accessibility_audit_after Rails.configuration.capybara_accessibility_audit.audit_after
     end
 
-    if defined?(RSpec)
-      RSpec.configure do |config|
-        config.include CapybaraAccessibilityAudit::AuditSystemTestExtensions, type: :system
-        config.include CapybaraAccessibilityAudit::AuditSystemTestExtensions, type: :feature
+    config.after_initialize do
+      if defined?(RSpec)
+        RSpec.configure do |config|
+          config.include CapybaraAccessibilityAudit::AuditSystemTestExtensions, type: :system
+          config.include CapybaraAccessibilityAudit::AuditSystemTestExtensions, type: :feature
 
-        configure = proc do
-          self.accessibility_audit_enabled = Rails.configuration.capybara_accessibility_audit.audit_enabled
+          configure = proc do
+            self.accessibility_audit_enabled = Rails.configuration.capybara_accessibility_audit.audit_enabled
 
-          accessibility_audit_after Rails.configuration.capybara_accessibility_audit.audit_after
+            accessibility_audit_after Rails.configuration.capybara_accessibility_audit.audit_after
+          end
+
+          config.before(type: :system, &configure)
+          config.before(type: :feature, &configure)
         end
-
-        config.before(type: :system, &configure)
-        config.before(type: :feature, &configure)
       end
     end
   end
